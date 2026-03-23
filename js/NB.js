21e2b219b0493c07445f5363593aead77a1f1055
luo <islxhere@163.com>
2026-03-23 17:16:04 +0800

    initial

--- js/NB.js
+++ js/NB.js
@@ -21,31 +21,31 @@ obj = {
 };
 $done({ body: JSON.stringify(obj) });
 
-var obj = JSON.parse($response.body);
+// var obj = JSON.parse($response.body);
 
-obj = {
-    "data": {
-        "processAppleReceipt": {
-            "__typename": "SubscriptionResult",
-            "error": 0,
-            "subscription": {
-                "__typename": "AppStoreSubscription",
-                "status": "canceled",
-                "originalPurchaseDate": "2023-04-19T03:11:16.000Z",
-                "originalTransactionId": "570001184068302",
-                "expirationDate": "9999-05-19T03:11:15.000Z",
-                "productId": "com.gingerlabs.Notability.premium_subscription",
-                "tier": "premium",
-                "refundedDate": null,
-                "refundedReason": null,
-                "isInBillingRetryPeriod": false,
-                "gracePeriodExpiresAt": null,
-                "expirationIntent": "CUSTOMER_CANCELLED",
-                "overDeviceLimit": false,
-                "user": null
-            },
-            "isClassic": false
-        }
-    }
-}
-$done({body: JSON.stringify(obj)});
+// obj = {
+//     "data": {
+//         "processAppleReceipt": {
+//             "__typename": "SubscriptionResult",
+//             "error": 0,
+//             "subscription": {
+//                 "__typename": "AppStoreSubscription",
+//                 "status": "canceled",
+//                 "originalPurchaseDate": "2023-04-19T03:11:16.000Z",
+//                 "originalTransactionId": "570001184068302",
+//                 "expirationDate": "9999-05-19T03:11:15.000Z",
+//                 "productId": "com.gingerlabs.Notability.premium_subscription",
+//                 "tier": "premium",
+//                 "refundedDate": null,
+//                 "refundedReason": null,
+//                 "isInBillingRetryPeriod": false,
+//                 "gracePeriodExpiresAt": null,
+//                 "expirationIntent": "CUSTOMER_CANCELLED",
+//                 "overDeviceLimit": false,
+//                 "user": null
+//             },
+//             "isClassic": false
+//         }
+//     }
+// }
+// $done({body: JSON.stringify(obj)});
